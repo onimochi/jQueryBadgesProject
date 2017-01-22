db5b15dd222904af71b55aa72e7c88f92f290b4c
onimochi <onimochi@excite.co.jp>
2017-01-22 17:59:19 +0900

    add ajax function that get badges in main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
-
-  // your code will go here
-
+    $.ajax({
+    url: 'https://www.codeschool.com/users/3305732.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      const badges = response.courses.completed;
+      for (let i in badges){
+        $('#badges').append(
+          `<div class="course">
+            <h3>${badges[i].title}</h3>
+            <img src="${badges[i].badge}">
+            <a class="btn btn-primary" href="${badges[i].url}" target="_blank">See Course</a>
+          </div>`
+        )
+      }
+    }
+  });
 });
